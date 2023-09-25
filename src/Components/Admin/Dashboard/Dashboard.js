30422aae9423a3defe2ecdf394b04d3c20a0db7b
Nadeem78633 <nadeemz7860@gmail.com>
2023-09-25 12:47:45 +0530

    all

--- src/Components/Admin/Dashboard/Dashboard.js
+++ src/Components/Admin/Dashboard/Dashboard.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Grid, Card, CardContent, Typography, Avatar } from "@mui/material";
-
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+  Box,
+} from "@mui/material";
 
 import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
+import Divider from "@mui/material/Divider";
 
 import { NavLink } from "react-router-dom";
 import AddUser from "./AddUser";
 import { useAuthState } from "react-firebase-hooks/auth";
 const Dashboard = () => {
-
-  
   return (
     <>
       <Grid
@@ -22,6 +28,205 @@ const Dashboard = () => {
           paddingRight: "5%",
         }}
       >
+        {/* Total Users Amount Investment */}
+        <Divider
+          style={{
+            width: "100%",
+
+            marginLeft: "1.7%",
+            border: "1px solid rgba(187, 196, 206, 0.35)",
+            marginBottom: "15px",
+          }}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Grid item xs={4} md={4} sm={4}>
+            <Card style={{ boxShadow: "none" }}>
+              <CardContent
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center", // Center vertically
+                }}
+              >
+                <Avatar
+                  style={{
+                    width: "45px",
+                    height: "45px",
+                    background: "rgba(187, 196, 206, 0.35)",
+                  }}
+                >
+                  <PeopleOutlineIcon
+                    style={{ width: "30px", height: "30px", color: "black" }}
+                  />
+                </Avatar>
+                <div>
+                  <Typography
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingLeft: "10px",
+                      fontSize: "14px",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      color: "#8789A3",
+                    }}
+                  >
+                    Total Users
+                  </Typography>
+                  <Typography
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingLeft: "15px",
+                      fontSize: "14px",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      color: "#8789A3",
+                    }}
+                  >
+                    100+
+                  </Typography>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Divider
+            orientation="vertical"
+            variant="middle"
+            flexItem
+            style={{ border: "1px solid rgba(187, 196, 206, 0.35)" }}
+          />
+          <Grid item xs={4} md={4} sm={4}>
+            <Card style={{ boxShadow: "none" }}>
+              <CardContent
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center", // Center vertically
+                }}
+              >
+                <Avatar
+                  style={{
+                    width: "45px",
+                    height: "45px",
+                    background: "rgba(187, 196, 206, 0.35)",
+                  }}
+                >
+                  <PeopleOutlineIcon
+                    style={{ width: "30px", height: "30px", color: "black" }}
+                  />
+                </Avatar>
+                <div>
+                  <Typography
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingLeft: "10px",
+                      fontSize: "14px",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      color: "#8789A3",
+                    }}
+                  >
+                    Total Users
+                  </Typography>
+                  <Typography
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingLeft: "15px",
+                      fontSize: "14px",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      color: "#8789A3",
+                    }}
+                  >
+                    100+
+                  </Typography>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Divider
+            orientation="vertical"
+            variant="middle"
+            flexItem
+            style={{ border: "1px solid rgba(187, 196, 206, 0.35)" }}
+          />
+          <Grid item xs={4} md={4} sm={4}>
+            <Card style={{ boxShadow: "none" }}>
+              <CardContent
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center", // Center vertically
+                }}
+              >
+                <Avatar
+                  style={{
+                    width: "45px",
+                    height: "45px",
+                    background: "rgba(187, 196, 206, 0.35)",
+                  }}
+                >
+                  <PeopleOutlineIcon
+                    style={{ width: "30px", height: "30px", color: "black" }}
+                  />
+                </Avatar>
+                <div>
+                  <Typography
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingLeft: "10px",
+                      fontSize: "14px",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      color: "#8789A3",
+                    }}
+                  >
+                    Total Users
+                  </Typography>
+                  <Typography
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      paddingLeft: "15px",
+                      fontSize: "14px",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      color: "#8789A3",
+                    }}
+                  >
+                    100+
+                  </Typography>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Box>
+        <Divider
+          style={{
+            width: "100%",
+            marginTop: "15px",
+            marginLeft: "1.7%",
+            border: "1px solid rgba(187, 196, 206, 0.35)",
+          }}
+        />
+
         <Grid item xs={6} md={6} sm={6}>
           <Card>
             <CardContent
